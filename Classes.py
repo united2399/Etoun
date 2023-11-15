1094c0b8ec5b2327a7176bae81e7df5541d4e05e
united2399 <115886821+united2399@users.noreply.github.com>
2023-11-15 12:12:21 +0100

    Add files via upload

---
+++ Classes.py
@@ -0,0 +1,35 @@
+# Operator class
+
+class Operator:
+    def __init__(self, name, health, attack):
+        self.name = name
+        self.health = health
+        self.maxhealth = health
+        self.attack = attack
+
+    def __str__(self):
+        return f"Name: {self.name}, Health: {self.health}, Attack: {self.attack}"
+
+    def print_name(self):
+        print(self.name)
+
+    def reset(self):
+      self.health = self.maxhealth
+
+# Enemy class
+
+class Enemy:
+    def __init__(self, name, health, attack):
+        self.name = name
+        self.health = health
+        self.maxhealth = health
+        self.attack = attack
+
+    def __str__(self):
+        return f"Name: {self.name}, Health: {self.health}, Attack: {self.attack}"
+
+    def print_name(self):
+        print(self.name)
+
+    def reset(self):
+        self.health = self.maxhealth
---
+++ main.py
@@ -0,0 +1,79 @@
+# Imports
+import time
+import random
+import math
+import colorama
+import os
+
+
+# From imports
+from colorama import Fore, Back, Style
+from Classes import Operator, Enemy
+from Utils import clear
+
+
+
+# Setting up the modules
+colorama.init()
+
+
+# Operators
+
+sand = Operator("Sand", 300, 300)
+
+# Enemies
+
+slug = Enemy("Slug", 600, 0)
+
+menu = "main"
+
+while True:
+  # Clearing the screen. stopping infinite printing bullshit
+  clear()
+  time.sleep (0.05)
+
+  # Main menu
+  while menu == "main":
+    clear()
+    time.sleep (0.05)
+    print ("(1) Campaign")
+    print ("(2) Recruitment")
+    print ("(3) Squads")
+    print ("(4) Operators")
+    e = input("")
+    if e == "1":
+      menu = "campaign"
+    elif e == "2":
+      menu = "recruitment"
+    elif e == "3":
+      menu = "squads"
+    elif e == "4":
+      menu = "operators"
+
+  # Campaign
+  while menu == "campaign":
+    clear()
+    time.sleep (0.05)
+    print ("(x) Main menu")
+    print ("(1) Plot or something 1")
+    e = input("")
+    if e == "x":
+      menu = "main"
+
+  # Recruitment
+  while menu == "recruitment":
+    clear()
+    time.sleep (0.05)
+    pass
+
+  # Squads
+  while menu == "squads":
+    clear()
+    time.sleep (0.05)
+    pass
+
+  # Operators
+  while menu == "operators":
+    clear()
+    time.sleep (0.05)
+    pass
