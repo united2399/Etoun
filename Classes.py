3cac40e49ba1cc61359cef8639834c4f7544a62d
united2399 <115886821+united2399@users.noreply.github.com>
2023-11-16 13:00:01 +0100

    Update Classes.py
    added a Spec variable to the operator class

--- Classes.py
+++ Classes.py
@@ -1,14 +1,15 @@
 # Operator class
 
 class Operator:
-    def __init__(self, name, health, attack):
+    def __init__(self, name, spec, health, attack):
         self.name = name
+        self.spec = spec
         self.health = health
         self.maxhealth = health
         self.attack = attack
 
     def __str__(self):
-        return f"Name: {self.name}, Health: {self.health}, Attack: {self.attack}"
+        return f"Name: {self.name}, Job: {self.spec} Health: {self.health}, Attack: {self.attack}"
 
     def print_name(self):
         print(self.name)
